58923b2decb4385105289e5a17b46c9c14881e8a
lejlailles <il970322@gmail.com>
2021-04-13 13:11:20 +0200

    Kettes feladat:js készítők neve, középre igazítás,betűméret betöltéskor

---
+++ kecske.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", init);
+
+
+function init() {
+var element = document.getElementById("keszitok");
+element.innerHTML = "Illés Lejla, Ménesi Csaba, Kovács Attila";
+element.style.textAlign = "center";
+element.style.fontSize="20px";
+   
+}
+
